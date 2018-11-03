1fa1eb13bee84050452e2e18eb257c7dd609f23d
konstntu17 <el15046@central.ntua.gr>
2018-11-04 00:10:16 +0200

    Added models except Volunteer

--- cheapiesgr/models.py
+++ cheapiesgr/models.py
@@ -2,29 +2,60 @@ from django.db import models
 
 # Create your models here.
 
-class Registration(models.Model):
-	pass
 
 class Volunteer(models.Model):
-	pass
+#   check out User Model:
+#       https://wsvincent.com/django-referencing-the-user-model/
+#   check out RegistrationFormUniqueEmail
+    pass
 
-class Rating(models.Model):
-	Stars = models.IntegerField()
-	Validity_of_this_rate = models.IntegerField()
-	Rate_explanation = models.CharField(max_length = 1000)
-	Registration_id = models.ForeignKey(Registration, on_delete = models.CASCADE)
-	Volunteer_id = models.ForeignKey(Volunteer, on_delete = models.CASCADE)
-
-class Category2(models.Model):
-	Category2_discription = models.CharField(max_length = 200)
-
-class Category1(models.Model):
-	Category1_discription = models.CharField(max_length = 200)
-	Category2_id = models.ForeignKey(Category2, on_delete = models.CASCADE)
-
-class Category(models.Model):
-	Category_discription = models.CharField(max_length = 200)
-	Category1_id = models.ForeignKey(Category1, on_delete = models.CASCADE)
 
 class Shop(models.Model):
-	Location = models.CharField(max_length = 200)
+    location = models.CharField(max_length = 200) # check out GeoDjango
+
+
+class Category2(models.Model): # Highest level (abstract)
+    category2_description = models.CharField(max_length = 200)
+#   image = models.ImageField(...)
+
+
+class Category1(models.Model): # Mid level (sparse)
+    category1_description = models.CharField(max_length = 200)
+    category2 = models.ForeignKey(Category2, on_delete = models.CASCADE)
+#   image = models.ImageField(...)
+
+
+class Category(models.Model): # Lowest level (dense)
+    category_description = models.CharField(max_length = 200)
+    category1 = models.ForeignKey(Category1, on_delete = models.CASCADE)
+#   image = models.ImageField(...)
+
+
+class Registration(models.Model):
+    product_description = models.CharField(max_length = 200)
+    price = models.DecimalField(max_digits = 10, decimal_places = 2)
+#   image = models.ImageField(...)
+    date_of_registration = models.DateField()
+    volunteer = models.ForeignKey(Volunteer, on_delete = models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete = models.CASCADE)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+
+
+class Rating(models.Model):
+    stars = models.IntegerField()
+    validity_of_this_rate = models.IntegerField()
+    rate_explanation = models.CharField(max_length = 1000)
+    registration = models.ForeignKey(Registration, on_delete = models.CASCADE)
+    volunteer = models.ForeignKey(Volunteer, on_delete = models.CASCADE)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length = 2000)
+    registration = models.ForeignKey(Registration, on_delete = models.CASCADE)
+
+
+class Answer(models.Model):
+    answer_text = models.CharField(max_length = 2000)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+
+
