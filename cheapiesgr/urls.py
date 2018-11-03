e060d825eeceda8276158768c44d2adbdddfd24b
Marios Papachristou <papachristoumarios@gmail.com>
2018-11-03 12:17:07 +0200

    Add Django REST Framework requirement

--- cheapiesgr/urls.py
+++ cheapiesgr/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'map', views.default_map, name="map"),
-    url(r'signup', views.signup, name="signup"),
-    url(r'', views.index, name="index"),
+    url(r'map', views.default_map, name='map'),
+    url(r'signup', views.signup, name='signup'),
+    url(r'signin', views.signin, name='signin'),
+    url(r'product', views.product, name='product'),
+    url(r'privacy', views.privacy, name='privacy'),
+    url(r'', views.index, name='index'),
 ]
--- cheapiesgr/views.py
+++ cheapiesgr/views.py
@@ -9,5 +9,15 @@ def default_map(request):
 def signup(request):
     return render(request, 'signup.html', {})
 
+def privacy(request):
+    return render(request, 'privacy.html', {})
+
+def signin(request):
+    return render(request, 'signin.html', {})
+
 def index(request):
-    return render(request, 'index.html', {})    
+    return render(request, 'index.html', {})
+
+def product(request):
+    return render(request, 'product.html',
+                  { 'mapbox_access_token' : settings.MAPBOX_ACCESS_TOKEN })
