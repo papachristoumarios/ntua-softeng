1cf520f516cfc816a181c0d413bbf78aef0eb1f8
jim113 <xd4ever7@gmail.com>
2018-11-03 06:11:12 -0700

    Half Part of DB

--- cheapiesgr/models.py
+++ cheapiesgr/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Registration(models.Model):
+	pass
+
+class Volunteer(models.Model):
+	pass
+
+class Rating(models.Model):
+	Stars = models.IntegerField()
+	Validity_of_this_rate = models.IntegerField()
+	Rate_explanation = models.CharField(max_length = 1000)
+	Registration_id = models.ForeignKey(Registration, on_delete = models.CASCADE)
+	Volunteer_id = models.ForeignKey(Volunteer, on_delete = models.CASCADE)
+
+class Category2(models.Model):
+	Category2_discription = models.CharField(max_length = 200)
+
+class Category1(models.Model):
+	Category1_discription = models.CharField(max_length = 200)
+	Category2_id = models.ForeignKey(Category2, on_delete = models.CASCADE)
+
+class Category(models.Model):
+	Category_discription = models.CharField(max_length = 200)
+	Category1_id = models.ForeignKey(Category1, on_delete = models.CASCADE)
