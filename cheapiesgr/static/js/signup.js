e2026f8292554c260d83b5c85fc7df7244236b5b
Marios Papachristou <papachristoumarios@gmail.com>
2018-11-09 12:32:04 +0200

    Reordered static files under cheapiesgr/static/

---
+++ cheapiesgr/static/js/signup.js
@@ -0,0 +1,23 @@
+var strength = {
+  0: "Απαίσια",
+  1: "Κακή",
+  2: "Αδύναμη",
+  3: "Καλή",
+  4: "Πολύ δυνατή"
+}
+
+var password = document.getElementById('pwd');
+var text = document.getElementById('password-strength-text');
+
+password.addEventListener('input', function()
+{
+  var val = password.value;
+  var result = zxcvbn(val);
+
+  if(val !== "") {
+    text.innerHTML = "Δύναμη Κωδικού: " + strength[result.score];
+  }
+  else {
+    text.innerHTML = "";
+  }
+});
