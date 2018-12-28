10b677c8cdf0285a761cbe3d4fd6a67d75261f05
Marios Papachristou <papachristoumarios@gmail.com>
2018-12-29 01:15:13 +0200

    Add MEDIA_ROOT

--- cheapies/settings.py
+++ cheapies/settings.py
@@ -15,8 +15,8 @@ import os
 from django.conf import settings
 from django.conf.urls.static import static
 
-#MEDIA_ROOT = os.path.abspath('cheapiesgr/media')
-#MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.abspath('cheapiesgr/static/media')
+MEDIA_URL = '/media/'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
