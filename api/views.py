68eb368b4c4efd9cc42ae5730227a7e3e859e0fd
Giannis Daras <daras.giannhs@gmail.com>
2018-12-28 22:33:21 +0200

    Initialize api application

---
+++ api/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+def login(request):
+    return None
+
+
+def logout(request):
+    return None
+
+
+def get_products(request):
+    return None
+
+def get_shops(request):
+    return None
+
+
+def get_prices(request):
+    return None
