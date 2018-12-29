997b01eb82d5a1a99e37c5bcefb800e852a64e6a
Marios Papachristou <papachristoumarios@gmail.com>
2018-12-30 00:37:31 +0200

    Format views to pep8

--- cheapiesgr/views.py
+++ cheapiesgr/views.py
@@ -22,28 +22,35 @@ import random
 import os
 nom = Nominatim()
 
+
 def handle_uploaded_file(f, category_name):
     category_name = category_name.replace(' ', '-')
     fname = '{}_{}'.format(random.randint(0, 100), str(f))
-    dest = 'media/supermarket_crawlers/{}/images/{}'.format(category_name, fname)
+    dest = 'media/supermarket_crawlers/{}/images/{}'.format(
+        category_name, fname)
     with open(os.path.join(settings.MEDIA_ROOT, dest), 'wb+') as g:
         g.write(f.read())
 
     return dest
 
+
 def default_map(request):
     return render(request, 'map_default.html', {})
+
 
 def profile(request):
     return render(request, 'profile.html', {})
 
+
 def privacy(request):
     return render(request, 'privacy.html', {})
+
 
 def index(request):
     request.session['categories'] = list(Category.objects.all().values())
 
     return render(request, 'index.html', {})
+
 
 def product(request):
     product_id = int(request.GET.get('productId', 0))
@@ -51,38 +58,43 @@ def product(request):
     lon = request.session.get('lon', 0)
     client_loc = Point(lon, lat, srid=4326)
 
-
     product = Registration.objects.get(pk=product_id)
     product_loc = product.location
 
     return render(request, 'product.html', {
-        'lat' : lat,
-        'lon' : lon,
-        'product' : product,
-        'plat' : product_loc.y,
-        'plon' : product_loc.x,
-        'distance' : distance(product.location , client_loc)
+        'lat': lat,
+        'lon': lon,
+        'product': product,
+        'plat': product_loc.y,
+        'plon': product_loc.x,
+        'distance': distance(product.location, client_loc)
     })
+
 
 def location(ip):
     g = GeoIP2()
     return g.geos(ip)
 
+
 def distance(a, b):
     meters = geopy_distance(a, b).meters
     return Distance(m=meters).km
 
+
 def order_by_distance(results):
-    results.sort(key = lambda x: x[1])
+    results.sort(key=lambda x: x[1])
     return results
 
+
 def order_by_rating(results):
-    results.sort(key = lambda x: 0 if x[0].stars == None else -x[0].stars)
+    results.sort(key=lambda x: 0 if x[0].stars is None else -x[0].stars)
     return results
+
 
 def apply_search_filters(results, dmax, rmin):
     results = filter(lambda x: x[0].stars >= rmin, results)
     return list(results)
+
 
 def infinite_scroll_paginator(paginator):
     for i in range(1, paginator.num_pages + 1):
@@ -105,7 +117,8 @@ def search(request):
         search_text = ''
         lat = request.session.get('lat', 0)
         lon = request.session.get('lon', 0)
-        reg_data = Registration.objects.filter(category__id=category_id)[:limit]
+        reg_data = Registration.objects.filter(
+            category__id=category_id)[:limit]
         client_loc = Point(lon, lat, srid=4326)
     else:
         search_text = request.POST.get('search')
@@ -124,7 +137,6 @@ def search(request):
         finally:
             client_loc = Point(lon, lat, srid=4326)
 
-
         try:
             orderby = request.POST.get('orderby')
         except ValueError:
@@ -136,7 +148,6 @@ def search(request):
             rmin = 0
         except TypeError:
             rmin = 0
-
 
         try:
             pmin = float(request.POST.get('pmin'))
@@ -166,11 +177,10 @@ def search(request):
         except TypeError:
             limit = -1
 
-
-
-        reg_data = Registration.objects.filter(product_description__contains=search_text,
-                        price__gte=pmin,
-                        price__lte=pmax)
+        reg_data = Registration.objects.filter(
+            product_description__contains=search_text,
+            price__gte=pmin,
+            price__lte=pmax)
 
         if category != 'Όλες':
             reg_data = reg_data.filter(category__category_name=category)
@@ -181,8 +191,8 @@ def search(request):
         if orderby == 'price':
             reg_data = reg_data.order_by('price')
 
-    distances = [distance(r.location ,client_loc) for r in reg_data]
-    results = [(r,d) for r, d in zip(reg_data, distances)]
+    distances = [distance(r.location, client_loc) for r in reg_data]
+    results = [(r, d) for r, d in zip(reg_data, distances)]
 
     if orderby == 'rating':
         results = order_by_rating(results)
@@ -197,31 +207,35 @@ def search(request):
     # paginate
     paginator = Paginator(results, 20)
 
-
     return render(request, 'search.html', {
-        'rmin' : rmin,
-        'pmin' : pmin,
-        'pmax' : pmax,
-        'dmax' : dmax,
+        'rmin': rmin,
+        'pmin': pmin,
+        'pmax': pmax,
+        'dmax': dmax,
         'num_results': num_results,
-        'num_pages' : paginator.num_pages,
-        'pages' : infinite_scroll_paginator(paginator),
-        'search_text' : search_text,
-        'lat' : lat,
-        'lon' : lon
+        'num_pages': paginator.num_pages,
+        'pages': infinite_scroll_paginator(paginator),
+        'search_text': search_text,
+        'lat': lat,
+        'lon': lon
     })
+
 
 def report(request):
     return render(request, 'report.html', {})
 
+
 def newproduct1(request):
     return render(request, 'newproduct1.html', {})
+
 
 def newproduct2(request):
     return render(request, 'newproduct2.html', {})
 
+
 def newproduct3(request):
     return render(request, 'newproduct3.html', {})
+
 
 def addproduct(request):
     if request.method == 'POST':
@@ -233,8 +247,9 @@ def addproduct(request):
             new_location = f.cleaned_data['new_location']
             category_id = f.cleaned_data['category']
             category = Category.objects.get(pk=category_id)
-            date_of_registration=datetime.datetime.today().strftime('%Y-%m-%d')
-            image_url = handle_uploaded_file(request.FILES['img'], category.category_name)
+            date_of_registration = datetime.datetime.today().strftime('%Y-%m-%d')
+            image_url = handle_uploaded_file(
+                request.FILES['img'], category.category_name)
 
             if len(new_location) > 0:
                 print('Adding a new shop at', new_location)
@@ -249,8 +264,6 @@ def addproduct(request):
             else:
                 shop_id = f.cleaned_data['location']
                 shop = Shop.objects.get(pk=shop_id)
-
-
 
             # TODO Change volunteer
             volunteer = Volunteer.objects.get(pk=1)
@@ -275,21 +288,26 @@ def addproduct(request):
 
     return render(request, 'addproduct.html', {})
 
+
 def user_auth(request):
     return render(request, 'user_auth.html', {})
+
 
 def signup(request):
     if request.method == 'POST':
         f = UserRegistrationForm(request.POST)
         if f.is_valid():
-            #f.save()
-            messages.success(request, 'Ο λογαριασμός δημιουργήθηκε με επιτυχία!')
+            # f.save()
+            messages.success(
+                request, 'Ο λογαριασμός δημιουργήθηκε με επιτυχία!')
             return render(request, 'index.html', {})
     else:
         f = UserRegistrationForm()
     return render(request, 'signup.html', {'form': f})
 
-#User login view
+# User login view
+
+
 def signin(request):
     if request.method == 'POST':
         f = UserLoginForm(request.POST)
