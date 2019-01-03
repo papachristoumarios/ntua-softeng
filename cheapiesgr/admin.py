7cd5df2cb6950bfaac430c5a26c19d1f70171b11
Giannis Daras <daras.giannhs@gmail.com>
2019-01-03 23:01:06 +0200

    Improve PEP8

--- cheapiesgr/admin.py
+++ cheapiesgr/admin.py
@@ -2,11 +2,13 @@ from django.contrib.gis import admin
 from .models import *
 from django.contrib.gis.db import models as gis_models
 
-class PointAdmin(admin.OSMGeoAdmin):	#Class using OSMGeoadmin
-    default_lon = 2642481.7 #lon of Athens
-    default_lat = 4576976.96 #lat of Athens
-    map_width = 600  # Dimensions of the
-    map_height = 400 # map (pixels)
-    default_zoom = 11 #Appropriate zoom
 
-admin.site.register([Shop, Volunteer, Registration,Category],PointAdmin)
+class PointAdmin(admin.OSMGeoAdmin):
+    default_lon = 2642481.7  # lon of Athens
+    default_lat = 4576976.96  # lat of Athens
+    map_width = 600  # Dimensions of the
+    map_height = 400  # map (pixels)
+    default_zoom = 11  # Appropriate zoom
+
+
+admin.site.register([Shop, Volunteer, Registration, Category], PointAdmin)
