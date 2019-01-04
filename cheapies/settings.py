a7137eccb2ca857a3b4d073223601e4ac371e537
Marios Papachristou <papachristoumarios@gmail.com>
2019-01-04 02:07:18 +0200

    Enable password hashing

--- cheapies/settings.py
+++ cheapies/settings.py
@@ -105,12 +105,12 @@ DATABASES = {
 }
 
 # Password Hashing
-# PASSWORD_HASHERS = [
-#     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-#     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-#     'django.contrib.auth.hashers.Argon2PasswordHasher',
-#     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-# ]
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
 
 
 # Password validation
