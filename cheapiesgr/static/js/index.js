7f48efc3c5abd03a866f1339da8d7c220a66a94a
Marios Papachristou <papachristoumarios@gmail.com>
2019-01-06 15:12:22 +0200

    Implement speech recognition

--- cheapiesgr/static/js/index.js
+++ cheapiesgr/static/js/index.js
@@ -1,4 +1,4 @@
-
+// Geolocation
 var options = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -18,3 +18,38 @@ var options = {
   };
 
   navigator.geolocation.getCurrentPosition(success, error, options);
+
+// Speech Recognition with webkitSpeechRecognition API
+$('button[type!=submit]').click(function(event) { event.stopPropagation(); });
+
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+let finalTranscript = '';
+let recognition = new window.SpeechRecognition();
+
+recognition.interimResults = true;
+recognition.maxAlternatives = 10;
+recognition.continuous = true;
+recognition.lang = "el-GR";
+
+
+recognition.onresult = (event) => {
+  let interimTranscript = '';
+  for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+    let transcript = event.results[i][0].transcript;
+    if (event.results[i].isFinal) {
+      finalTranscript += transcript;
+    } else {
+      interimTranscript += transcript;
+    }
+  }
+
+  document.getElementById('search').value = finalTranscript + interimTranscript;
+}
+
+recognition.onspeechend = (event) => {
+  $("#search-form").submit();
+}
+
+function recognizeSpeech() {
+  recognition.start();
+}
