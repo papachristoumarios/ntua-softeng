c9a004bd605be73cd1e77f656402a8547a2c07ef
Marios Papachristou <papachristoumarios@gmail.com>
2019-02-19 17:42:41 +0200

    Add unicode response method

--- api/views.py
+++ api/views.py
@@ -1,5 +1,11 @@
+import json
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse, HttpResponse
+
+def unicode_response(data):
+    """ Returns unicode response """
+    return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json")
 
 @require_http_methods(['POST'])
 def login(request):
@@ -7,7 +13,7 @@ def login(request):
 
 @require_http_methods(['POST'])
 def logout(request):
-    return None
+    return unicode_response({'δσαFoo' : 'ok'})
 
 
 def get_products(request):
