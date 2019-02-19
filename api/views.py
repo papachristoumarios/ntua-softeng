6d16b01acec435849d6f8bf57a23442161378749
Marios Papachristou <papachristoumarios@gmail.com>
2019-02-19 17:30:15 +0200

    Add required http methods to login/logout methods

--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
-
+@require_http_methods(['POST'])
 def login(request):
     return None
 
-
+@require_http_methods(['POST'])
 def logout(request):
     return None
 
