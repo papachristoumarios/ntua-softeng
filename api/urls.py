19ed38d2b0b7b04fe0b33443804eb9676219884a
Marios Papachristou <papachristoumarios@gmail.com>
2019-02-20 22:17:45 +0200

    Add correct url to products

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,10 @@ from . import views
 urlpatterns = [
     url(r'login', obtain_auth_token, name='login'),
     url(r'logout', views.logout_user, name='logout'),
-    url(r'products', views.product, name='products'),
+    url(r'products', include([
+        url(r'/(?P<product_id>\w+)', views.product),
+        url(r'', views.product),
+    ])),
     url(r'prices', views.price, name='prices'),
     url(r'shops', include([
         url(r'/(?P<shop_id>\w+)', views.shop),
