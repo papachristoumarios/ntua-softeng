8e1756b9e3e21ad0123d4a758087c0de27908e1b
giannissiax <44746622+giannissiax@users.noreply.github.com>
2019-03-02 19:21:12 +0200

    tests added

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,10 @@
+from django.http import QueryDict
 import urllib
 import json
+import copy
 from django.test import TestCase
 from rest_framework.test import APIClient, APIRequestFactory
+from rest_framework import status
 from cheapiesgr.models import *
 from .views import AUTH_TOKEN_LABEL
 
@@ -12,7 +15,7 @@ def urldump(data):
     return urllib.parse.urlencode(data)
 
 class APITestcase(TestCase):
-
+    #me poion tropo ginontai ta test????
     def setUp(self):
         self.factory = APIRequestFactory()
         self.client = APIClient()
@@ -33,23 +36,67 @@ class APITestcase(TestCase):
         self.token = login_response['token']
         self.header = { AUTH_TOKEN_LABEL : self.token }
         self.client.credentials(HTTP_X_OBSERVATORY_AUTH=self.token)
-
-    def test_product(self):
-        product = {
+        self.product = {
             'name' : 'foo',
             'description' : 'foo',
             'category' : 'laptop',
             'tags' : ['a', 'b'],
             'withdrawn' : False
         }
-        response = self.client.post('/observatory/api/products', urldump(product), content_type="application/x-www-form-urlencoded")
+        self.products = [self.product]
+        response = self.client.post('/observatory/api/products/', urldump(self.product), content_type='application/x-www-form-urlencoded')
+        response = decode_response(response)
+        print(response)
+        self.idd = response['id']
+        response = self.client.get('/observatory/api/products/{}'.format(self.idd),format = 'json')
+        response = decode_response(response)
+        assert(response['name'] == self.product['name'])
+        assert(response['description'] == self.product['description'])
+        assert(response['category'] == self.product['category'])
+        assert(self.product['tags'] == response['tags'])
+        assert(response['withdrawn'] == self.product['withdrawn'])
+        assert(response['name'] == self.product['name'])
 
-        print(decode_response(response))
+
+    def test_get_products(self):
+        response = self.client.get('/observatory/api/products/',format = 'json')
+        response = decode_response(response)
+        assert(response['count'] == 20)
+        assert(response['products'][0]['name'] == self.product['name'])
+
+        assert(response['start'] == 0)
+        assert(response['total'] == 1)
+
+    def test_put_products(self):
+
+        newproduct = {
+            'name' : 'foo1',
+            'description' : 'foo1',
+            'category' : 'laptop1',
+            'tags' : ['a1', 'b1'],
+            'withdrawn' : False
+        }
+        response = self.client.put('/observatory/api/products/{}'.format(self.idd), urldump(newproduct), content_type='application/x-www-form-urlencoded')
+        response = self.client.get('/observatory/api/products/{}'.format(self.idd), format = 'json')
+        response = decode_response(response)
+        assert(response['name'] == newproduct['name'])
+        assert(response['description'] == newproduct['description'])
+        assert(response['category'] == newproduct['category'])
+        assert(response['tags'] == newproduct['tags'])
+        assert(response['withdrawn'] == newproduct['withdrawn'])
+
+        response = self.client.put('/observatory/api/products/{}'.format(self.idd), urldump(self.product), content_type='application/x-www-form-urlencoded')
+        
+    def test_patch_product(self):
+        response = self.client.patch('/observatory/api/products/{}'.format(self.idd),urldump({'name':'foo2','category' : 'laptop2'}), content_type='application/x-www-form-urlencoded')
+        response = self.client.get('/observatory/api/products/{}'.format(self.idd), format = 'json')
+        response = decode_response(response)
+        assert(response['name'] == 'foo2')
+        assert(response['description'] == 'laptop2')
+        response = self.client.put('/observatory/api/products/{}'.format(self.idd), urldump(self.product), content_type='application/x-www-form-urlencoded')
 
 
-    def logout(self):
-        print(self.header)
-
-        logout_response = self.client.post('/observatory/api/logout/', **self.header)
-        logout_response = decode_response(logout_response)
-        assert(logout_response['message'] == 'OK')
+    def test_delete_product(self):
+        response = self.client.delete('/observatory/api/products/{}'.format(self.idd))
+        response = decode_response(response)
+        assert(response['withdrawn'] == True)
